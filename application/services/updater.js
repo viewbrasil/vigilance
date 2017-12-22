5ed7a665b849e763374b7903d6309091e91c60e7
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-12-22 21:36:22 -0200

    bleh

--- application/services/updater.js
+++ application/services/updater.js
@@ -8,50 +8,48 @@ var utf8 = require('utf8');
 const prettier = require("prettier");
 var pretty = require('pretty');
 
-function Updater(path) {
+function Updater() {
 
-this.path = path;
-this.canQuery = true;
 }
 
-Updater.prototype.verify_git = function()
+Updater.prototype.verify_git = function(path,branch)
 {
-  path = this.path;
-  timed_check(path);
+  timed_check(path,branch);
 }
 
 
-function timed_check(path)
+function timed_check(path,branch)
 {
-    shell_exec(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"', false);
-    console.log(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"', false);
+    shell_exec(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"', false,branch);
+    //console.log(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"');
 }
 
-function work_on_response(response,path,stop)
+function work_on_response(response,path,stop,branch)
 {
 
     if(response.trim() == '"untracked"')
     {
       log.info('updating local files..');
-      shell_exec(path , ' git reset --hard && git pull origin master', true);
+      shell_exec(path , ' git reset --hard origin/'+branch+' && git pull origin ' + branch, true,branch);
+      //console.log(path , ' git reset --hard origin/'+branch+' && git pull origin ' + branch)
     }
     else {
       if(stop == true)
       {
         log.info('Update successful');
       }
-      timed_check(path);
+      timed_check(path,branch);
     }
-//haha
+
 }
 
-function shell_exec(path, command, stop)
+function shell_exec(path, command, stop,branch)
 {
   exec(path + command , function(error, stdout, stderr) {
 
     if (typeof error != null) {
 
-      work_on_response(stdout,path,stop);
+      work_on_response(stdout,path,stop,branch);
 
     }
   });
