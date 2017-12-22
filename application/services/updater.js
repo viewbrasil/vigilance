678f1b6fbc7e75c07d0b1b804ad658dfe799fae5
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-12-22 14:21:56 -0200

    Devmind commit in 2017-12-22 14:21:55

--- application/services/updater.js
+++ application/services/updater.js
@@ -28,7 +28,7 @@ function timed_check(path)
 function work_on_response(response,path)
 {
 
-    //aquele teste maroto eae
+    //aquele teste maroto eae loco
   if(response.trim() == '"untracked"')
   {
     log.info('updating local files..');
