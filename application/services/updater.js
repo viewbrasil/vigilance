9db3e50d03ed48427b6b8d7d859b9c2db1bd5bd2
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-12-25 12:08:15 -0200

    changes

--- application/services/updater.js
+++ application/services/updater.js
@@ -7,6 +7,7 @@ var fs = require('fs');
 var utf8 = require('utf8');
 const prettier = require("prettier");
 var pretty = require('pretty');
+const isOnline = require('is-online');
 
 function Updater() {
 
@@ -14,14 +15,34 @@ function Updater() {
 
 Updater.prototype.verify_git = function(path,branch)
 {
-  timed_check(path,branch);
+check_connection(path,branch);
+}
+
+function check_connection(path,branch)
+{
+  isOnline().then(online => {
+
+    if(online == true)
+    {
+      timed_check(path,branch);
+    }
+    else {
+
+    setTimeout(function(){
+
+      log.warning('connection lost. Trying again in 3 seconds.')
+      check_connection(path,branch)
+
+    }, 3000);
+    }
+
+  });
 }
 
 
 function timed_check(path,branch)
 {
     shell_exec(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"', false,branch);
-    //console.log(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"');
 }
 
 function work_on_response(response,path,stop,branch)
@@ -31,7 +52,6 @@ function work_on_response(response,path,stop,branch)
     {
       log.info('updating local files..');
       shell_exec(path , ' git reset --hard origin/'+branch+' && git pull origin ' + branch, true,branch);
-      //console.log(path , ' git reset --hard origin/'+branch+' && git pull origin ' + branch)
     }
     else {
       if(stop == true)
