0d9cf235dc54f5f64a11601f9a8ee1f7fa9e4a0b
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-12-26 10:15:58 -0200

    Devmind commit in 2017-12-26 10:15:57

--- application/services/updater.js
+++ application/services/updater.js
@@ -1,77 +1,67 @@
 module.exports = Updater;
-var exec = require('child_process').exec;
-var log = require('captains-log')();
-const isOnline = require('is-online');
+var exec = require("child_process").exec;
+var log = require("captains-log")();
+const isOnline = require("is-online");
 var lostConnection = false;
 
-function Updater() {
+function Updater() {}
 
-}
+Updater.prototype.verify_git = function(path, branch) {
+  check_connection(path, branch);
+};
 
-Updater.prototype.verify_git = function(path,branch)
-{
-check_connection(path,branch);
-}
-
-function check_connection(path,branch)
-{
+function check_connection(path, branch) {
   isOnline().then(online => {
-
-    if(online == true)
-    {
-      if(lostConnection == true)
-      {
-        log.info('Connection restablished.');
+    if (online == true) {
+      if (lostConnection == true) {
+        log.info("Connection restablished.");
         lostConnection = false;
       }
-      timed_check(path,branch);
+      timed_check(path, branch);
+    } else {
+      setTimeout(function() {
+        log.info("Connection lost. Trying again in 3 seconds.");
+        lostConnection = true;
+        check_connection(path, branch);
+      }, 3000);
     }
-    else {
-
-    setTimeout(function(){
-
-      log.info('Connection lost. Trying again in 3 seconds.');
-      lostConnection = true;
-      check_connection(path,branch)
-
-    }, 3000);
-    }
-
   });
 }
 
-
-function timed_check(path,branch)
-{
-    shell_exec(path , 'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"', false,branch);
+function timed_check(path, branch) {
+  shell_exec(
+    path,
+    'git fetch &&  git diff-index --quiet FETCH_HEAD -- || echo "untracked"',
+    false,
+    branch
+  );
 }
 
-function work_on_response(response,path,stop,branch)
-{
-
-    if(response.trim() == '"untracked"')
-    {
-      log.info('updating local files..');
-      shell_exec(path , ' git reset --hard origin/'+branch+' && git pull origin ' + branch + ' && pm2 stop start.js && pm2 start start.js', true,branch);
+function work_on_response(response, path, stop, branch) {
+  if (response.trim() == '"untracked"') {
+    log.info("updating local files..");
+    shell_exec(
+      path,
+      " git reset --hard origin/" +
+        branch +
+        " && git pull origin " +
+        branch +
+        " && pm2 stop start.js && pm2 start start.js",
+      true,
+      branch
+    );
+  } else {
+    if (stop == true) {
+      log.info("Update successful");
     }
-    else {
-      if(stop == true)
-      {
-        log.info('Update successful');
-      }
-      check_connection(path,branch);
-    }
-
+    check_connection(path, branch);
+  }
 }
 
-function shell_exec(path, command, stop,branch)
-{
-  exec(path + command , function(error, stdout, stderr) {
-
+function shell_exec(path, command, stop, branch) {
+  exec(path + command, function(error, stdout, stderr) {
     if (typeof error != null) {
-
-      work_on_response(stdout,path,stop,branch);
-
+      work_on_response(stdout, path, stop, branch);
     }
   });
 }
