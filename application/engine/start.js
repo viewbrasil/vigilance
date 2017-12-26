489def2941d24989f04afa7f1e0be41d3823b15d
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-12-26 14:24:02 -0200

    Devmind commit in 2017-12-26 14:24:01

--- application/engine/start.js
+++ application/engine/start.js
@@ -11,8 +11,6 @@ if (typeof args.path !== "undefined") {
     for (var i = 0; i < lines.length; i++) {
       log.info(lines[i]);
     }
-    
-    //hahaha
 
     log.info(" Devmind.io Vigilance is watching your files at " + args.path);
     log.info(" To shut down, press <CTRL> + C at any time.");
