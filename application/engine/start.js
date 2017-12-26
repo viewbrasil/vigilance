87a200e396d44302a8cb41912400d53232069236
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-12-26 10:50:37 -0200

    changes

--- application/engine/start.js
+++ application/engine/start.js
@@ -3,21 +3,22 @@ var args = require("minimist")(process.argv.slice(2));
 var fs = require("fs");
 var log = require("captains-log")();
 
-fs.readFile("./application/includes/startup.txt", function(err, file) {
-  var lines = file.toString().split("\n");
-  console.log("");
-  for (var i = 0; i < lines.length; i++) {
-    log.info(lines[i]);
-  }
-
-  log.info(" Devmind.io vigilance is running");
-  log.info(" Vigilance is watching your files at " + args.path);
-  log.info(" To shut down, press <CTRL> + C at any time.");
-  console.log("");
-});
-
 if (typeof args.path !== "undefined") {
   var command_prepend = "cd " + args.path + " && ";
+  fs.readFile("./application/includes/startup.txt", function(err, file) {
+    var lines = file.toString().split("\n");
+    console.log("");
+    for (var i = 0; i < lines.length; i++) {
+      log.info(lines[i]);
+    }
+
+    log.info(" Devmind.io vigilance is running");
+    log.info(" Vigilance is watching your files at " + args.path);
+    log.info(" To shut down, press <CTRL> + C at any time.");
+    console.log("");
+  });
+
+
   if (typeof args.branch !== "undefined") {
     var branch = args.branch;
   } else {
