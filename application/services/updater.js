fffa1e874758ae413f79b18362fb5f78c4343a39
Lucas Meine <lucas.meine@citara.io>
2018-04-04 00:28:56 -0300

    quick refactor

--- application/services/updater.js
+++ application/services/updater.js
@@ -3,7 +3,7 @@ var exec = require("child_process").exec;
 var log = require("captains-log")();
 const isOnline = require("is-online");
 var lostConnection = false;
-const notifier = require('node-notifier');
+const notifier = require("node-notifier");
 const pathModule = require("path");
 
 var commands = null;
@@ -13,38 +13,37 @@ function Updater() {}
 Updater.prototype.verify_git = function(path, branch, command) {
   commands = command;
 
-
-
   check_connection(path, branch);
 };
 
-function send_notification(notification_message){
-
-  notifier.notify(
-    {
-      title: 'Devmind Vigilance',
-      message: notification_message,
-      icon: pathModule.join(pathModule.resolve(__dirname, '..')+"/includes", 'devmind.png'), // Absolute path (doesn't work on balloons)
-      sound: true, // Only Notification Center or Windows Toasters
-      wait: false
-    });
-
+function send_notification(notification_message) {
+  notifier.notify({
+    title: "Devmind Vigilance",
+    message: notification_message,
+    icon: pathModule.join(
+      pathModule.resolve(__dirname, "..") + "/includes",
+      "devmind.png"
+    ), // Absolute path (doesn't work on balloons)
+    sound: true, // Only Notification Center or Windows Toasters
+    wait: false
+  });
 }
 
 function check_connection(path, branch) {
-
   isOnline().then(online => {
     if (online == true) {
       if (lostConnection == true) {
         log.info("Connection restablished.");
-        send_notification('Vigilance Connection restablished.');
+        send_notification("Vigilance Connection restablished.");
         lostConnection = false;
       }
       timed_check(path, branch);
     } else {
       setTimeout(function() {
         log.info("Connection lost. Trying again in 3 seconds.");
-        send_notification('Vigilance connection lost. Trying again in 3 seconds.');
+        send_notification(
+          "Vigilance connection lost. Trying again in 3 seconds."
+        );
         lostConnection = true;
         check_connection(path, branch);
       }, 3000);
@@ -53,18 +52,20 @@ function check_connection(path, branch) {
 }
 
 function timed_check(path, branch) {
-
   shell_exec(
     path,
-    'git fetch &&  git diff ' + branch+' origin/'+branch+' --quiet || echo "untracked"',
+    "git fetch &&  git diff " +
+      branch +
+      " origin/" +
+      branch +
+      ' --quiet || echo "untracked"',
     false,
     branch
   );
 }
 
 function work_on_response(response, path, stop, branch) {
-  
-  if (response.trim() == 'untracked' || response.trim() == '"untracked"') {
+  if (response.trim().includes("untracked")) {
     log.info("updating local files..");
     exec_str =
       " git reset --hard origin/" + branch + " && git pull origin " + branch;
@@ -78,19 +79,16 @@ function work_on_response(response, path, stop, branch) {
   } else {
     if (stop == true) {
       log.info("Update successful");
-      send_notification('Vigilance Update successful.');
+      send_notification("Vigilance Update successful.");
     }
     check_connection(path, branch);
   }
 }
 
 function shell_exec(path, command, stop, branch) {
-
   exec(path + command, function(error, stdout, stderr) {
-
     if (typeof error != null) {
-
-      work_on_response(stdout, path, stop,branch);
+      work_on_response(stdout, path, stop, branch);
     }
   });
 }
